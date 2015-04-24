a0c3f37de8dfff64ff9f2c9c25fe67359d4036b5
Ignacio Baixas <iobaixas@gmail.com>
2015-04-24 11:05:39 -0300

    feat(): second commiy

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,31 @@
-console.log('This would be the main JS file.');
+angular.module('reddit', ['restmod'])
+.config(function(restmodProvider) {
+	restmodProvider.rebase('BaseModel');
+})
+.run(function($rootScope) {
+
+})
+
+.factory('BaseModel', function(restmod) {
+	return restmod.mixin({
+		$extend: {
+			'Model.unpack': function(_resource, _raw) {
+				if(_raw.kind == 'Listing') {
+					return _raw.data.children;
+				} else {
+					throw 'not implemented';
+				}
+			}
+		}
+	});
+})
+.factory('Item', function(restmod) {
+    return restmod.model();
+})
+.factory('RedditRoot', function(restmod) {
+	return restmod.model().mix({
+		hot: {
+			has_many: 'Item'
+		}
+	}).single('/');
+});
